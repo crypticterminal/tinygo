f7275bec63d30be4abf9d67fcd5463ecc1879fbe
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-11-11 19:26:11 +0100

    machine: redesign I2C interface

--- src/machine/i2c.go
+++ src/machine/i2c.go
@@ -7,3 +7,24 @@ const (
 	TWI_FREQ_100KHZ = 100000
 	TWI_FREQ_400KHZ = 400000
 )
+
+// WriteRegister transmits first the register and then the data to the
+// peripheral device.
+//
+// Many I2C-compatible devices are organized in terms of registers. This method
+// is a shortcut to easily write to such registers.
+func (i2c I2C) WriteRegister(address uint8, register uint8, data []byte) {
+	buf := make([]uint8, len(data)+1)
+	buf[0] = register
+	copy(buf[1:], data)
+	i2c.Tx(uint16(address), buf, nil)
+}
+
+// ReadRegister transmits the register, restarts the connection as a read
+// operation, and reads the response.
+//
+// Many I2C-compatible devices are organized in terms of registers. This method
+// is a shortcut to easily read such registers.
+func (i2c I2C) ReadRegister(address uint8, register uint8, data []byte) {
+	i2c.Tx(uint16(address), []byte{register}, data)
+}
